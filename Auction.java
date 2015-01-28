75bcc811b3b1484674011e16dbd92a8e8957383f
martabajo <martabg2492@gmail.com>
2015-01-28 13:06:17 +0100

    Actividad 0060: añadido metodo close en Auction

--- Auction.java
+++ Auction.java
@@ -43,7 +43,7 @@ public class Auction
             System.out.println(lot.toString());
         }
     }
-    
+
     /**
      * Make a bid for a lot.
      * A message is printed indicating whether the bid is
@@ -61,14 +61,14 @@ public class Auction
             boolean successful = selectedLot.bidFor(bid);
             if(successful) {
                 System.out.println("The bid for lot number " +
-                                   lotNumber + " was successful.");
+                    lotNumber + " was successful.");
             }
             else {
                 // Report which bid is higher.
                 Bid highestBid = selectedLot.getHighestBid();
                 System.out.println("Lot number: " + lotNumber +
-                                   " already has a bid of: " +
-                                   highestBid.getValue());
+                    " already has a bid of: " +
+                    highestBid.getValue());
             }
         }
     }
@@ -87,9 +87,9 @@ public class Auction
             // right lot.
             if(selectedLot.getNumber() != lotNumber) {
                 System.out.println("Internal error: Lot number " +
-                                   selectedLot.getNumber() +
-                                   " was returned instead of " +
-                                   lotNumber);
+                    selectedLot.getNumber() +
+                    " was returned instead of " +
+                    lotNumber);
                 // Don't return an invalid lot.
                 selectedLot = null;
             }
@@ -97,10 +97,34 @@ public class Auction
         }
         else {
             System.out.println("Lot number: " + lotNumber +
-                               " does not exist.");
+                " does not exist.");
             return null;
         }
-    }
-    
 
+    } 
+
+    /**
+     * Actividad 0060: Añade un metodo close.
+     */
+    public void close()
+    {
+        for (Lot item : lots)
+        {
+
+            System.out.println(item.toString());
+
+            if (item.getHighestBid() != null)
+            {
+                System.out.println("El nombre de la persona que ha pujado mas alto es: " + item.getHighestBid().getBidder().getName());
+
+            }
+            else
+            {
+                System.out.println("El articulo no ha recibido pujas.");
+            }
+
+        }
+
+    }
 }
+    
